37850a50a2a1ef1fb8ae690f795d7acc3f097f35
Maksym Vitkovskyi <clevercattv@gmail.com>
2020-10-15 00:10:25 +0300

    Change createStudent return type

--- src/main/java/com/clevercattv/student/list/controller/StudentController.java
+++ src/main/java/com/clevercattv/student/list/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.clevercattv.student.list.dto.CreateStudentRequest;
 import com.clevercattv.student.list.entity.Student;
 import com.clevercattv.student.list.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/student")
 @RequiredArgsConstructor
+@Validated
 public class StudentController {
 
     private final StudentService service;
@@ -24,8 +28,8 @@ public class StudentController {
         return service.readAll();
     }
 
-    @PostMapping
-    public Mono<Long> getSecond(@RequestBody CreateStudentRequest createRequest) {
+    @PostMapping(consumes = "application/json")
+    public Mono<Student> createStudent(@Valid @RequestBody CreateStudentRequest createRequest) {
         return service.create(createRequest);
     }
 
--- src/main/java/com/clevercattv/student/list/service/StudentService.java
+++ src/main/java/com/clevercattv/student/list/service/StudentService.java
@@ -6,21 +6,24 @@ import com.clevercattv.student.list.repository.StudentRepository;
 import com.clevercattv.student.list.service.mapper.StudentMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class StudentService {
 
     private final StudentRepository repository;
     private final StudentMapper mapper;
 
-    public Mono<Long> create(CreateStudentRequest createRequest) {
+    public Mono<Student> create(CreateStudentRequest createRequest) {
         Student student = mapper.toEntity(createRequest);
-        return repository.save(student).map(Student::getId);
+        return repository.save(student);
     }
 
+    @Transactional(readOnly = true)
     public Flux<Student> readAll() {
         return repository.findAll();
     }

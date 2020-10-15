df8b2f7da23945b3179b720bd22d76020c7ab5f8
Maksym Vitkovskyi <clevercattv@gmail.com>
2020-10-15 17:28:11 +0300

    Add createStudent_EmptyRequest test. Refactoring

--- src/test/java/com/clevercattv/student/list/controller/StudentControllerValidationTest.java
+++ src/test/java/com/clevercattv/student/list/controller/StudentControllerValidationTest.java
@@ -3,8 +3,10 @@ package com.clevercattv.student.list.controller;
 import com.clevercattv.student.list.config.R2dbcConfig;
 import com.clevercattv.student.list.config.WebFluxConfig;
 import com.clevercattv.student.list.dto.CreateStudentRequest;
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -17,6 +19,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
@@ -32,6 +36,13 @@ class StudentControllerValidationTest {
     private static final String SIZE_NAME_MESSAGE = "Incorrect length! Allowed length from 2 to 26";
     private static final String SIZE_UNIVERSITY_MESSAGE = "Incorrect length! Allowed length from 2 to 63";
     private static final String PATTERN_MESSAGE = "Incorrect symbols! Allowed symbols a-zA-Z ,.'-";
+
+    private static final String JSON_PATH_FIRST_NAME = "$.firstName";
+    private static final String JSON_PATH_LAST_NAME = "$.lastName";
+    private static final String JSON_PATH_UNIVERSITY = "$.university";
+    private static final String JSON_PATH_SPECIALTY = "$.specialty";
+    private static final String JSON_PATH_SEMESTER = "$.semester";
+    private static final String JSON_PATH_AGE = "$.age";
 
     private static final CreateStudentRequest.CreateStudentRequestBuilder VALID_REQUEST_BUILDER = CreateStudentRequest.builder()
             .firstName("Anabal")
@@ -51,7 +62,28 @@ class StudentControllerValidationTest {
         client = WebTestClient.bindToApplicationContext(context).build();
     }
 
-    @ParameterizedTest(name = "[{index}] [{1}] messages {2} request {0}")
+    @Test
+    void createStudent_emptyRequest_ReturnValidationMessage() {
+        Matcher<List<String>> notNullMatcher = Matchers.is(Collections.singletonList(NOT_NULL_MESSAGE));
+
+        client.post()
+                .uri("/student")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(CreateStudentRequest.builder().build()), CreateStudentRequest.class)
+                .exchange()
+                .expectStatus()
+                .is4xxClientError()
+                .expectBody()
+                .jsonPath(JSON_PATH_FIRST_NAME).value(notNullMatcher)
+                .jsonPath(JSON_PATH_LAST_NAME).value(notNullMatcher)
+                .jsonPath(JSON_PATH_UNIVERSITY).value(notNullMatcher)
+                .jsonPath(JSON_PATH_SPECIALTY).value(notNullMatcher)
+                .jsonPath(JSON_PATH_SEMESTER).value(notNullMatcher)
+                .jsonPath(JSON_PATH_AGE).value(notNullMatcher);
+    }
+
+    @ParameterizedTest(name = "{index} [{1}] messages {2} request {0}")
     @MethodSource({
             "invalidFirstNameRequestArguments", "invalidLastNameRequestArguments",
             "invalidUniversityRequestArguments", "invalidSpecialtyRequestArguments",
@@ -68,6 +100,7 @@ class StudentControllerValidationTest {
                 .expectStatus()
                 .is4xxClientError()
                 .expectBody()
+                .jsonPath("$.*").value(Matchers.hasSize(1))
                 .jsonPath(path).value(Matchers.containsInAnyOrder(messages));
     }
 
@@ -75,37 +108,37 @@ class StudentControllerValidationTest {
         return Stream.of(
                 Arguments.of( // null
                         buildRequest(request -> request.setFirstName(null)), // change valid param to invalid
-                        "$.firstName[*]", // jsonPath
+                        JSON_PATH_FIRST_NAME,
                         new String[]{NOT_NULL_MESSAGE} // validation messages
                 ),
                 Arguments.of( // Empty value
                         buildRequest(request -> request.setFirstName("")),
-                        "$.firstName[*]",
+                        JSON_PATH_FIRST_NAME,
                         new String[]{SIZE_NAME_MESSAGE}
                 ),
                 Arguments.of( // Min size
                         buildRequest(request -> request.setFirstName("a")),
-                        "$.firstName[*]",
+                        JSON_PATH_FIRST_NAME,
                         new String[]{SIZE_NAME_MESSAGE}
                 ),
                 Arguments.of( // Pattern
                         buildRequest(request -> request.setFirstName("a$")),
-                        "$.firstName[*]",
+                        JSON_PATH_FIRST_NAME,
                         new String[]{PATTERN_MESSAGE}
                 ),
                 Arguments.of( // Pattern + Min size
                         buildRequest(request -> request.setFirstName("$")),
-                        "$.firstName[*]",
+                        JSON_PATH_FIRST_NAME,
                         new String[]{PATTERN_MESSAGE, SIZE_NAME_MESSAGE}
                 ),
                 Arguments.of( // Max size
                         buildRequest(request -> request.setFirstName("Hector Sausage-Hausen King.")),
-                        "$.firstName[*]",
+                        JSON_PATH_FIRST_NAME,
                         new String[]{SIZE_NAME_MESSAGE}
                 ),
                 Arguments.of( // Pattern + Max size
                         buildRequest(request -> request.setFirstName("Hector# Sausage-Hausen King.")),
-                        "$.firstName[*]",
+                        JSON_PATH_FIRST_NAME,
                         new String[]{PATTERN_MESSAGE, SIZE_NAME_MESSAGE}
                 )
         );
@@ -115,37 +148,37 @@ class StudentControllerValidationTest {
         return Stream.of(
                 Arguments.of( // null
                         buildRequest(request -> request.setLastName(null)), // change valid param to invalid
-                        "$.lastName[*]", // jsonPath
+                        JSON_PATH_LAST_NAME,
                         new String[]{NOT_NULL_MESSAGE} // validation messages
                 ),
                 Arguments.of( // Empty value
                         buildRequest(request -> request.setLastName("")),
-                        "$.lastName[*]",
+                        JSON_PATH_LAST_NAME,
                         new String[]{SIZE_NAME_MESSAGE}
                 ),
                 Arguments.of( // Min size
                         buildRequest(request -> request.setLastName("a")),
-                        "$.lastName[*]",
+                        JSON_PATH_LAST_NAME,
                         new String[]{SIZE_NAME_MESSAGE}
                 ),
                 Arguments.of( // Pattern
                         buildRequest(request -> request.setLastName("a$")),
-                        "$.lastName[*]",
+                        JSON_PATH_LAST_NAME,
                         new String[]{PATTERN_MESSAGE}
                 ),
                 Arguments.of( // Pattern + Min size
                         buildRequest(request -> request.setLastName("$")),
-                        "$.lastName[*]",
+                        JSON_PATH_LAST_NAME,
                         new String[]{PATTERN_MESSAGE, SIZE_NAME_MESSAGE}
                 ),
                 Arguments.of( // Max size
                         buildRequest(request -> request.setLastName("Hector Sausage-Hausen King.")),
-                        "$.lastName[*]",
+                        JSON_PATH_LAST_NAME,
                         new String[]{SIZE_NAME_MESSAGE}
                 ),
                 Arguments.of( // Pattern + Max size
                         buildRequest(request -> request.setLastName("Hector# Sausage-Hausen King.")),
-                        "$.lastName[*]",
+                        JSON_PATH_LAST_NAME,
                         new String[]{PATTERN_MESSAGE, SIZE_NAME_MESSAGE}
                 )
         );
@@ -155,37 +188,37 @@ class StudentControllerValidationTest {
         return Stream.of(
                 Arguments.of( // null
                         buildRequest(request -> request.setUniversity(null)), // change valid param to invalid
-                        "$.university[*]", // jsonPath
+                        JSON_PATH_UNIVERSITY,
                         new String[]{NOT_NULL_MESSAGE} // validation messages
                 ),
                 Arguments.of( // Empty value
                         buildRequest(request -> request.setUniversity("")),
-                        "$.university[*]",
+                        JSON_PATH_UNIVERSITY,
                         new String[]{SIZE_UNIVERSITY_MESSAGE}
                 ),
                 Arguments.of( // Min size
                         buildRequest(request -> request.setUniversity("a")),
-                        "$.university[*]",
+                        JSON_PATH_UNIVERSITY,
                         new String[]{SIZE_UNIVERSITY_MESSAGE}
                 ),
                 Arguments.of( // Pattern
                         buildRequest(request -> request.setUniversity("a$")),
-                        "$.university[*]",
+                        JSON_PATH_UNIVERSITY,
                         new String[]{PATTERN_MESSAGE}
                 ),
                 Arguments.of( // Pattern + Min size
                         buildRequest(request -> request.setUniversity("$")),
-                        "$.university[*]",
+                        JSON_PATH_UNIVERSITY,
                         new String[]{PATTERN_MESSAGE, SIZE_UNIVERSITY_MESSAGE}
                 ),
                 Arguments.of( // Max size
                         buildRequest(request -> request.setUniversity("The University of Manchester The University of Manchester Manchester")),
-                        "$.university[*]",
+                        JSON_PATH_UNIVERSITY,
                         new String[]{SIZE_UNIVERSITY_MESSAGE}
                 ),
                 Arguments.of( // Pattern + Max size
                         buildRequest(request -> request.setUniversity("The University# of Manchester The University of Manchester Manchester")),
-                        "$.university[*]",
+                        JSON_PATH_UNIVERSITY,
                         new String[]{PATTERN_MESSAGE, SIZE_UNIVERSITY_MESSAGE}
                 )
         );
@@ -195,37 +228,37 @@ class StudentControllerValidationTest {
         return Stream.of(
                 Arguments.of( // null
                         buildRequest(request -> request.setSpecialty(null)), // change valid param to invalid
-                        "$.specialty[*]", // jsonPath
+                        JSON_PATH_SPECIALTY,
                         new String[]{NOT_NULL_MESSAGE} // validation messages
                 ),
                 Arguments.of( // Empty value
                         buildRequest(request -> request.setSpecialty("")),
-                        "$.specialty[*]",
+                        JSON_PATH_SPECIALTY,
                         new String[]{SIZE_UNIVERSITY_MESSAGE}
                 ),
                 Arguments.of( // Min size
                         buildRequest(request -> request.setSpecialty("a")),
-                        "$.specialty[*]",
+                        JSON_PATH_SPECIALTY,
                         new String[]{SIZE_UNIVERSITY_MESSAGE}
                 ),
                 Arguments.of( // Pattern
                         buildRequest(request -> request.setSpecialty("a$")),
-                        "$.specialty[*]",
+                        JSON_PATH_SPECIALTY,
                         new String[]{PATTERN_MESSAGE}
                 ),
                 Arguments.of( // Pattern + Min size
                         buildRequest(request -> request.setSpecialty("$")),
-                        "$.specialty[*]",
+                        JSON_PATH_SPECIALTY,
                         new String[]{PATTERN_MESSAGE, SIZE_UNIVERSITY_MESSAGE}
                 ),
                 Arguments.of( // Max size
                         buildRequest(request -> request.setSpecialty("The University of Manchester The University of Manchester Manchester")),
-                        "$.specialty[*]",
+                        JSON_PATH_SPECIALTY,
                         new String[]{SIZE_UNIVERSITY_MESSAGE}
                 ),
                 Arguments.of( // Pattern + Max size
                         buildRequest(request -> request.setSpecialty("The University# of Manchester The University of Manchester Manchester")),
-                        "$.specialty[*]",
+                        JSON_PATH_SPECIALTY,
                         new String[]{PATTERN_MESSAGE, SIZE_UNIVERSITY_MESSAGE}
                 )
         );
@@ -235,17 +268,17 @@ class StudentControllerValidationTest {
         return Stream.of(
                 Arguments.of( // null
                         buildRequest(request -> request.setSemester(null)), // change valid param to invalid
-                        "$.semester[*]", // jsonPath
+                        JSON_PATH_SEMESTER,
                         new String[]{NOT_NULL_MESSAGE} // validation messages
                 ),
                 Arguments.of( // Min size
                         buildRequest(request -> request.setSemester(0)),
-                        "$.semester[*]",
+                        JSON_PATH_SEMESTER,
                         new String[]{MIN_SEMESTER_MESSAGE}
                 ),
                 Arguments.of( // Max size
                         buildRequest(request -> request.setSemester(21)),
-                        "$.semester[*]",
+                        JSON_PATH_SEMESTER,
                         new String[]{MAX_SEMESTER_MESSAGE}
                 )
         );
@@ -255,17 +288,17 @@ class StudentControllerValidationTest {
         return Stream.of(
                 Arguments.of( // null
                         buildRequest(request -> request.setAge(null)), // change valid param to invalid
-                        "$.age[*]", // jsonPath
+                        JSON_PATH_AGE,
                         new String[]{NOT_NULL_MESSAGE} // validation messages
                 ),
                 Arguments.of( // Min size
                         buildRequest(request -> request.setAge(0)),
-                        "$.age[*]",
+                        JSON_PATH_AGE,
                         new String[]{MIN_AGE_MESSAGE}
                 ),
                 Arguments.of( // Max size
                         buildRequest(request -> request.setAge(201)),
-                        "$.age[*]",
+                        JSON_PATH_AGE,
                         new String[]{MAX_AGE_MESSAGE}
                 )
         );

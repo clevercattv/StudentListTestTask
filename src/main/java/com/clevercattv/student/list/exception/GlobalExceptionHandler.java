a104673a8a4e889f7fffe48d32766befc9ea2e64
Maksym Vitkovskyi <clevercattv@gmail.com>
2020-10-15 17:02:26 +0300

    Add ServerWebInputException handler

--- src/main/java/com/clevercattv/student/list/exception/GlobalExceptionHandler.java
+++ src/main/java/com/clevercattv/student/list/exception/GlobalExceptionHandler.java
@@ -9,7 +9,9 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.support.WebExchangeBindException;
+import org.springframework.web.server.ServerWebInputException;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -21,10 +23,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({WebExchangeBindException.class})
     public ResponseEntity<Object> handleValidationException(WebExchangeBindException exception) {
         log.error("WebExchangeBindException", exception);
+
         if (!exception.getGlobalErrors().isEmpty()) {
-            return handleUnexpectedException(exception);
-        }
-        if (exception.getFieldErrors().isEmpty()) {
             return handleUnexpectedException(exception);
         }
         Map<String, List<String>> response = exception.getFieldErrors()
@@ -35,10 +35,20 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, new HttpHeaders(), exception.getStatus());
     }
 
+    @ExceptionHandler({ServerWebInputException.class})
+    public ResponseEntity<Object> handleValidationException(ServerWebInputException exception) {
+        log.error("ServerWebInputException", exception);
+
+        String reason = exception.getReason();
+        return new ResponseEntity<>(
+                Collections.singletonMap("error", reason.substring(0, reason.indexOf(':'))),
+                new HttpHeaders(), exception.getStatus());
+    }
+
     @ExceptionHandler({Exception.class})
     public ResponseEntity<Object> handleUnexpectedException(Exception exception) {
-
         log.error("UnexpectedException", exception);
+
         return new ResponseEntity<>(exception.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 

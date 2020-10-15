60535fa2a1324b1c8e50799a609b3a86ec93e0c4
Maksym Vitkovskyi <clevercattv@gmail.com>
2020-10-15 15:58:02 +0300

    Add StudentController logs

--- src/main/java/com/clevercattv/student/list/controller/StudentController.java
+++ src/main/java/com/clevercattv/student/list/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.clevercattv.student.list.dto.CreateStudentRequest;
 import com.clevercattv.student.list.entity.Student;
 import com.clevercattv.student.list.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/student")
 @RequiredArgsConstructor
+@Slf4j
 @Validated
 public class StudentController {
 
@@ -25,11 +27,13 @@ public class StudentController {
 
     @GetMapping
     public Flux<Student> readAll() {
+        log.info("call GET [/student]");
         return service.readAll();
     }
 
     @PostMapping(consumes = "application/json")
     public Mono<Student> createStudent(@Valid @RequestBody CreateStudentRequest createRequest) {
+        log.info("call POST [/student]");
         return service.create(createRequest);
     }
 
